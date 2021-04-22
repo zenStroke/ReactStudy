b081ba388ce07ef99887bfb8251128f0438d2e0f
LuckyWind_sck <luckywindsck@gmail.com>
2021-04-22 23:25:08 +0900

    add HelloMessage component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HelloMessage from './HelloMessage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <HelloMessage />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
